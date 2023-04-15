b787f98f5c3a96fa14d7b0802eed919404b852f8
harmlessman <harmlessman17@gmail.com>
2023-04-15 23:57:59 +0900

    Add flatten and print_info

--- PAFTS/datasets/dataset.py
+++ PAFTS/datasets/dataset.py
@@ -1,38 +1,33 @@
 from pathlib import Path
 
-from pydub import AudioSegment
-
-AUDIO_FILE_EXT = [
-    '.wav',
-    '.mp3',
-    '.raw',
-    '.pcm',
-    '.mp4',
-    '.mkv'
-]
-
-
-def is_audio(path):
-    file = Path(path)
-    if file.suffix in AUDIO_FILE_EXT:
-        return True
-    return False
-
-
-def get_duration(path):
-    audio = AudioSegment.from_file(path)
-    return audio.duration_seconds
+from PAFTS.utils.data_info import is_audio, get_duration
+from PAFTS.utils.file_utils import rmtree
 
 
 class Dataset:
+    """
+    Audio Dataset Class.
+    Process all audio files in path.
+
+    Args:
+        path (str): Directory path with audio files.
+        dataset_name (str, optional): Dataset name. Defaults to dataset_path's directory name.
+        language (str, optional): Language using BCP 47 language tag. Defaults to 'en-us' (English)
+
+    """
     def __init__(
             self,
-            path: str = "",
-            dataset_name: str = "",
-            output_path: str = "",
-            language: str = "",
+            path: str,
+            dataset_name: str = None,
+            language: str = "en-us",
 
     ):
+        if not Path(path).exists():
+            raise FileNotFoundError("[!] Path does not exist")
+
+        if not Path(path).is_dir():
+            raise NotADirectoryError("[!] Path is not directory.")
+
         self.path = Path(path)
 
         if dataset_name:
@@ -40,53 +35,82 @@ class Dataset:
         else:
             self.dataset_name = Path(path).name
 
-        if output_path:
-            self.output_path = output_path
-        else:
-            self.output_path = Path(path).joinpath('output')
-
         self.language = language
-
-        self.items = self.get_audio_file()
-
-    def get_total_duration(self):
-        total_duration = 0
-        file_list = [p for p in self.path.glob("**/*") if is_audio(p)]
-
-        for file in file_list:
-            total_duration += get_duration(file)
-        return total_duration
 
     def get_audio_file(self):
         return [p for p in self.path.glob("**/*") if is_audio(p)]
 
-    def get_file_num(self):
-        total_num = 0
-        for p in self.path.glob('**/*'):
-            if is_audio(p):
-                total_num += 1
-        return total_num
+    def get_total_duration(self):
+        total_duration = 0
+        items = self.get_audio_file()
 
-    def update_item(self):
-        self.items = self.get_audio_file()
-        return self.items
+        for item in items:
+            total_duration += get_duration(item)
+        return total_duration
+
+    def get_file_num(self):
+        return len(self.get_audio_file())
 
     def __len__(self):
-        return len(self.items)
+        return self.get_file_num()
 
-    def __getitem__(self, idx):
-        return self.items[idx]
+    def flatten(self):
+        """
+        Flatten directory structure.
 
+        Example :
 
-if __name__ == '__main__':
-    d = Dataset('C:\\Users\\82109\\Desktop\\PAFTS\\data')
-    print(d.get_total_duration())
-    print(len(d))
-    print(d.path)
-    print(d.dataset_name)
-    print(d.output_path)
-    print(d.language)
-    print(d.get_file_num())
-    print(d.get_audio_file())
-    print(d.update_item())
-    print(d.items)
+            before dataset structure
+
+              path
+                ├── a
+                │   ├── 1.wav
+                │   ├── 2.wav
+                │   └── 3.wav
+                ├── b
+                │   ├── 1.wav
+                │   └── 2.wav
+                ├── 1.wav
+                ├── 2.wav
+                └── c
+                    └── d
+                        └── 1.wav
+
+            after dataset structure
+
+                  path
+                    ├── a_1.wav
+                    ├── a_2.wav
+                    ├── a_3.wav
+                    ├── b_1.wav
+                    ├── b_2.wav
+                    ├── 1.wav
+                    ├── 2.wav
+                    └── c_d_1.wav
+
+        """
+        items = self.get_audio_file()
+
+        for item in items:
+            p = item.parent
+            if p.samefile(self.path):
+                name = item.name
+            else:
+                front_list = str(p.relative_to(self.path)).split('\\')
+                name = '_'.join((front_list + [item.name]))
+
+            item.rename(self.path / name)
+
+        # remove empty directory
+        dir_list = [i for i in self.path.glob('*') if i.is_dir()]
+
+        for d in dir_list:
+            rmtree(d)
+
+    def print_info(self):
+        print(f'| > Dataset name : {self.dataset_name}')
+        print(f'| > Path : {self.path}')
+        print(f'| > language : {self.language}')
+        print(f'| > Number of files : {self.get_file_num()}')
+        print(f'| > Total duration : {self.get_total_duration()}')
+        print()
